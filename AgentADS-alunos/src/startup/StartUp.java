98e366c3620bb271352784bc13bef349b49136e1
dramataparty <101320231+dramataparty@users.noreply.github.com>
2023-05-26 11:57:24 +0100

    Startpoint

---
+++ AgentADS-alunos/src/startup/StartUp.java
@@ -0,0 +1,21 @@
+package startup;
+
+import javafx.application.Application;
+import view.MainView;
+/**
+ * Classe que implementa o caso de uso startup usando uma
+ * interface com o utilizador grafica
+ * @author fmartins
+ *
+ */
+public class StartUp {
+
+	public static void main(String[] args) {
+		// Quando ja' quiserem testar a aplicacao sobre o vosso
+		// objeto inicial usam a seguinte instrucao em vez da outra:
+		// MainView.setApp(new domain.OCT());
+		MainView.setApp(new domain.stubs.OCT());
+		Application.launch(MainView.class);
+	}
+
+}
