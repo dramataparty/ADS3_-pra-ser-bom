9b456e10333e940edc985b53f8f0fc3778202855
gabrielprojeto01 <62442346+gabrielprojeto01@users.noreply.github.com>
2023-05-28 18:55:27 +0100

    Update MissionInfoSearchedAlert.java

--- AgentADS-alunos/src/domain/MissionInfoSearchedAlert.java
+++ AgentADS-alunos/src/domain/MissionInfoSearchedAlert.java
@@ -1,19 +1,39 @@
 package domain;
 
+/**
+ * A classe MissionInfoSearchedAlert representa um alerta de informações de missão pesquisadas.
+ * Implementa a interface Alert.
+ */
 public class MissionInfoSearchedAlert implements Alert {
     private String subject;
     private String message;
 
+    /**
+     * Construtor da classe MissionInfoSearchedAlert.
+     * 
+     * @param subject O assunto do alerta.
+     * @param message A mensagem do alerta.
+     */
     public MissionInfoSearchedAlert(String subject, String message) {
         this.subject = subject;
         this.message = message;
     }
 
+    /**
+     * Retorna o nome do assunto do alerta.
+     * 
+     * @return O nome do assunto do alerta.
+     */
     @Override
     public String getSubjectName() {
         return subject;
     }
 
+    /**
+     * Retorna a mensagem do alerta.
+     * 
+     * @return A mensagem do alerta.
+     */
     @Override
     public String getMessage() {
         return message;
