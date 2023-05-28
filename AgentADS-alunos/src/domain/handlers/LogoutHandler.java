3ef3afa233b7b65e1355a3825949f7e53bd73f98
Dramata <diogobailaoforte@gmail.com>
2023-05-28 18:47:25 +0100

    fug

---
+++ AgentADS-alunos/src/domain/handlers/LogoutHandler.java
@@ -0,0 +1,20 @@
+package domain.handlers;
+
+import domain.User;
+import domain.UserCatalog;
+
+public class LogoutHandler implements ILogoutHandler {
+    private UserCatalog userCatalog;
+
+    public LogoutHandler(UserCatalog userCatalog) {
+        this.userCatalog = userCatalog;
+    }
+
+    @Override
+    public void logout(String username) {
+        User user = userCatalog.getUserByUsername(username);
+        if (user != null) {
+            user.setLoggedIn(false);
+        }
+    }
+}
