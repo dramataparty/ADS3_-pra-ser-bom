608d37d1ccf548aa10c8e7f5ed1c3adc07177186
gabrielprojeto01 <62442346+gabrielprojeto01@users.noreply.github.com>
2023-05-28 18:11:51 +0100

    Update IdentityCodifierStrategy.java

--- AgentADS-alunos/src/domain/IdentityCodifierStrategy.java
+++ AgentADS-alunos/src/domain/IdentityCodifierStrategy.java
@@ -1,5 +1,30 @@
 package domain;
 
-public class IdentityCodifierStrategy {
+/**
+ * Uma estratégia de codificação e decodificação baseada na identidade, onde o texto codificado/descodificado
+ * permanece igual ao texto fornecido como parâmetro.
+ */
+public class IdentityCodifierStrategy implements CodifierStrategy {
 
+    /**
+     * Codifica o texto fornecido e o retorna sem alterações.
+     *
+     * @param text o texto a ser codificado
+     * @return o texto codificado (que é igual ao texto de entrada)
+     */
+    @Override
+    public String code(String text) {
+        return text;
+    }
+
+    /**
+     * Decodifica o texto fornecido e o retorna sem alterações.
+     *
+     * @param text o texto a ser decodificado
+     * @return o texto decodificado (que é igual ao texto de entrada)
+     */
+    @Override
+    public String decode(String text) {
+        return text;
+    }
 }
