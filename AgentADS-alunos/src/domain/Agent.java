527395cf06077f6611e24fe879a58818e33ab92e
gabrielprojeto01 <62442346+gabrielprojeto01@users.noreply.github.com>
2023-05-28 19:04:19 +0100

    Update Agent.java

--- AgentADS-alunos/src/domain/Agent.java
+++ AgentADS-alunos/src/domain/Agent.java
@@ -1,12 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Classe que representa um agente.
+ */
 class Agent {
     private String codeName;
     private ICodifierStrategy codifierStrategy;
     private List<Document> documents;
     private Availability state;
 
+    /**
+     * Construtor da classe Agent.
+     *
+     * @param codeName O nome do agente.
+     */
     public Agent(String codeName) {
         this.codeName = codeName;
         this.codifierStrategy = new MyCodifierStrategy();
@@ -14,6 +22,11 @@ class Agent {
         this.state = Availability.AVAILABLE;
     }
 
+    /**
+     * Obtém o nome do codificador utilizado pelo agente.
+     *
+     * @return O nome do codificador.
+     */
     public String getCodifierName() {
         String name = codifierStrategy.getName();
         String message = "Agent " + codeName + " codifier name was searched";
@@ -21,6 +34,11 @@ class Agent {
         return name;
     }
 
+    /**
+     * Obtém as referências dos documentos do agente.
+     *
+     * @return As referências dos documentos.
+     */
     public Iterable<String> documentReferences() {
         List<String> references = new ArrayList<>();
         for (Document document : documents) {
@@ -31,6 +49,12 @@ class Agent {
         return references;
     }
 
+    /**
+     * Obtém o texto decodificado de um documento com base na referência.
+     *
+     * @param ref A referência do documento.
+     * @return O texto decodificado do documento.
+     */
     public Iterable<String> decodedDocText(String ref) {
         List<String> decodedText = new ArrayList<>();
         for (Document document : documents) {
@@ -47,6 +71,11 @@ class Agent {
         return decodedText;
     }
 
+    /**
+     * Torna o agente indisponível.
+     *
+     * @param unavail O estado de indisponibilidade do agente.
+     */
     public void becomeUnavailable(String unavail) {
         try {
             state = Availability.valueOf(unavail);
@@ -57,14 +86,29 @@ class Agent {
         }
     }
 
+    /**
+     * Adiciona um documento à lista de documentos do agente.
+     *
+     * @param document O documento a ser adicionado.
+     */
     public void addDocument(Document document) {
         documents.add(document);
     }
 
+    /**
+     * Obtém o estado atual do agente.
+     *
+     * @return O estado do agente.
+     */
     public Availability getState() {
         return state;
     }
 
+    /**
+     * Retorna uma representação em forma de string do agente.
+     *
+     * @return A representação em string do agente.
+     */
     @Override
     public String toString() {
         return "Agent: " + codeName + ", State: " + state;
